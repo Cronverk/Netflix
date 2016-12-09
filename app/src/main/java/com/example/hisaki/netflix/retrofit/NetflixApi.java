c4a88cc98a61e5502a14886ee41209893d640169
Cronverk <pozniack@gmail.com>
2016-12-09 15:15:27 +0200

    add retrofit

---
+++ app/src/main/java/com/example/hisaki/netflix/retrofit/NetflixApi.java
@@ -0,0 +1,25 @@
+package com.example.hisaki.netflix.retrofit;
+
+
+import com.example.hisaki.netflix.enteties.Movie;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by hisaki on 08.12.2016.
+ */
+
+public interface NetflixApi {
+    @GET("api.php")
+    Call<Movie> getMovieByTitle(@Query("title") String title);
+
+    @POST("api.php")
+    Call<List<Movie>> getMovieByDirector(@Path("director") String director);
+}
